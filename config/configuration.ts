0ea938ce262819ae0e2d9201c29728ffc1267158
Shtymak <vova.shtimak@gmail.com>
2022-11-23 22:01:05 +0200

    Add new env variables

--- config/configuration.ts
+++ config/configuration.ts
@@ -6,5 +6,19 @@ export const config = () => ({
   },
   jwt: {
     secret: process.env.JWT_SECRET || 'secret',
+    expiresIn: process.env.JWT_EXPIRES_IN || '7d',
+  },
+  hostUrl: process.env.HOST_URL || 'http://localhost:9999',
+  mail: {
+    host: process.env.MAIL_HOST || 'smtp.ethereal.email',
+    port: parseInt(process.env.MAIL_PORT, 10) || 587,
+    secure: process.env.MAIL_SECURE === 'true' || false,
+    auth: {
+      user: process.env.MAIL_AUTH_USER || 'user',
+      pass: process.env.MAIL_AUTH_PASS || 'pass',
+    },
+    defaults: {
+      from: process.env.MAIL_DEFAULTS_FROM || 'user',
+    },
   },
 });
--- config/validation.ts
+++ config/validation.ts
@@ -5,4 +5,12 @@ export const validationSchema = Joi.object({
   BASE_URL: Joi.string().default('http://localhost'),
   DATABASE_URI: Joi.string().default('mongodb://localhost:27017/nestjs'),
   JWT_SECRET: Joi.string().default('secret'),
+  JWT_EXPIRES_IN: Joi.string().default('7d'),
+  HOST_URL: Joi.string().default('http://localhost:9999'),
+  MAIL_HOST: Joi.string().default('smtp.ethereal.email'),
+  MAIL_PORT: Joi.number().default(587),
+  MAIL_SECURE: Joi.boolean().default(false),
+  MAIL_AUTH_USER: Joi.string().default('user'),
+  MAIL_AUTH_PASS: Joi.string().default('pass'),
+  MAIL_DEFAULTS_FROM: Joi.string().default('user'),
 });
