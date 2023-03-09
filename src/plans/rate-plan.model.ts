e28ef67b8ac3eff641e717cfde4122b9a3684b5d
Shtymak <vova.shtimak@gmail.com>
2023-03-09 22:35:02 +0200

    Avaible status for rate_plan

--- src/plans/rate-plan.model.ts
+++ src/plans/rate-plan.model.ts
@@ -3,20 +3,45 @@ export interface StoragePlan {
   storageLimit: number; // in GB
   price: number; // in USD per month
   isFree: boolean;
+  avaible: boolean;
 }
 export const StoragePlans: StoragePlan[] = [
-  { name: 'base', storageLimit: 5, price: 0, isFree: true },
-  { name: 'student', storageLimit: 100, price: 0, isFree: true },
-  { name: 'move', storageLimit: 15, price: 1, isFree: false },
+  { name: 'base', storageLimit: 5, price: 0, isFree: true, avaible: true },
+  { name: 'student', storageLimit: 100, price: 0, isFree: true, avaible: true },
+  { name: 'move', storageLimit: 15, price: 1, isFree: false, avaible: true },
   // { name: 'move+', storageLimit: 25, price: 1.5, isFree: false },
-  { name: 'pro', storageLimit: 50, price: 2, isFree: false },
-  { name: 'pro+', storageLimit: 100, price: 3, isFree: false },
-  { name: 'set', storageLimit: 500, price: 10, isFree: false },
+  { name: 'pro', storageLimit: 50, price: 2, isFree: false, avaible: true },
+  { name: 'pro+', storageLimit: 100, price: 3, isFree: false, avaible: true },
+  { name: 'set', storageLimit: 500, price: 10, isFree: false, avaible: false },
   // { name: 'set+', storageLimit: 1000, price: 18, isFree: false },
-  { name: 'crazy', storageLimit: 1800, price: 25, isFree: false },
-  { name: 'crazy+', storageLimit: 3000, price: 35, isFree: false },
-  { name: 'sensei', storageLimit: 5000, price: 50, isFree: false },
-  { name: 'sensei+', storageLimit: 10000, price: 75, isFree: false },
+  {
+    name: 'crazy',
+    storageLimit: 1800,
+    price: 25,
+    isFree: false,
+    avaible: false,
+  },
+  {
+    name: 'crazy+',
+    storageLimit: 3000,
+    price: 35,
+    isFree: false,
+    avaible: false,
+  },
+  {
+    name: 'sensei',
+    storageLimit: 5000,
+    price: 50,
+    isFree: false,
+    avaible: false,
+  },
+  {
+    name: 'sensei+',
+    storageLimit: 10000,
+    price: 75,
+    isFree: false,
+    avaible: false,
+  },
 ];
 
 export enum StoragePlanView {
